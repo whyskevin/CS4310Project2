ba0fc7081881e2cc747b639fd00b3c881c7ead07
LocNess <alexlocsu@gmail.com>
2019-03-22 20:22:29 +0000

    removed mmu variable from CPU class

--- CPU.java
+++ CPU.java
@@ -5,14 +5,12 @@ import java.lang.Math;
 public class CPU {
 	public final File test_data = new File("/Project2_test_and_page_files.test_files");
 	public static File [] listOfFiles = null;
-	public MMU mmu;
 
-	public CPU(int tlbSize, int numVirtualPages, int numPhysicalPages, int pageSize) {
+	public CPU() {
 		listOfFiles = test_data.listFiles();
-		mmu = new MMU(tlbSize, numVirtualPages, numPhysicalPages, pageSize);
 	}
 	
-	public void readProcesses() throws FileNotFoundException{
+	public void readProcesses(MMU mmu) throws FileNotFoundException{
 		boolean read = false;
 		int data = 0;
 		
@@ -27,11 +25,8 @@ public class CPU {
 
 					//grabbing virtual address and splitting
 					String virtualAddress = in.next();
-					String virtualPageStr = virtualAddress.substring(0,2);
-					String pageOffsetStr = virtualAddress.substring(2,2);
-
-					int virtualPage = convertHexToDecimal(virtualPageStr);
-					int pageOffset = convertHexToDecimal(pageOffsetStr);
+					int virtualPage = Integer.parseInt(virtualAddress.substring(0,2), 16);
+					int pageOffset = Integer.parseInt(virtualAddress.substring(2,2), 16);
 
 					boolean write = instruction == 1;
 
@@ -44,27 +39,5 @@ public class CPU {
 			
 			}
 		}
-	}
-	public int convertHexToDecimal(String hex) {
-		int decimal = 0;		
-
-		for (int i = 0; i < hex.size(); ++i) {
-			char hexChar = hex[hex.size()-1-i];
-			int offset = '0';
-			if (hexChar >= 'A' && hexChar <= 'F')
-				offset = 'A';
-			
-			decimal += Math.pow(16,i) * (hexChar - offset)
-		}
-
-		return decimal;
-	}
-
-	public void resetRbits() {
-		mmu.resetRbits();
-	}
-
-	public void unsetDbit(int virtualPageIndex) {
-		mmu.unsetDbit(virtualPageIndex);
 	}
 }
