44674e58303a15d6c189e83d0370e23f7b0b9d60
sonal-p <sonalperiwal1@gmail.com>
2019-03-22 17:34:08 -0700

    Add files via upload

--- OS.java
+++ OS.java
@@ -2,51 +2,55 @@ import java.io.FileNotFoundException;
 
 public class OS {
 	
-	private static CircularlyLinkedList<TlbEntries> clock = new CircularlyLinkedList();
+	private static CircularlyLinkedList<VirtualPageTableEntries> clock = new CircularlyLinkedList();
 	
-	public static CircularlyLinkedList<TlbEntries> loadClock(MMU mmu){
-		TlbEntries[] rtnTLB = new TlbEntries[mmu.getPhysicalMem().length];
-		for(TlbEntries i:rtnTLB) {
+	public static CircularlyLinkedList<VirtualPageTableEntries> loadClock(MMU mmu){
+		VirtualPageTableEntries[] rtnPT = mmu.getPageTable();
+		for(VirtualPageTableEntries i:rtnPT) {
 			clock.add(i);
 		}
 		return clock;
+		
+		
 	}
 	
-	public static TlbEntries pageReplacement(TlbEntries replacementEntry) {
+	public static VirtualPageTableEntries pageReplacement(VirtualPageTableEntries replacementEntry) {
 		if(clock.getData().isReferenced()) {
-			TlbEntries rtn = clock.getData();
+			VirtualPageTableEntries rtn = clock.getData();
 			clock.setData(replacementEntry);
 			return rtn;
 		} else {
 			clock.getData().setRbit(false);
+			//changePageTableEntry(clock.getData(), mmu);
 			clock.advance();
 		}
 		return clock.getData();
 	}
 	
-	public static void clockEvict(TlbEntries replacementEntry) throws FileNotFoundException {
-		TlbEntries t = pageReplacement(replacementEntry);
+	/*public static void chagePageTableEntry(TlbEntries entry, MMU mmu) {
+		int virtualPageIndex = entry.getVirtualPageNum();
+		mmu.getPageTable()[virtualPageIndex].setRbit(false);
+	}*/
+	
+	public static void clockEvict(VirtualPageTableEntries replacementEntry) throws FileNotFoundException {
+		VirtualPageTableEntries t = pageReplacement(replacementEntry);
 		if(t.isDirty()) {
 			//write to hard disk?
 			diskWrite(t);
 		}
 	}
 	
-	public static void diskWrite(TlbEntries t) throws FileNotFoundException {
+	public static void diskWrite(VirtualPageTableEntries t) throws FileNotFoundException {
 		
 	}
 	
-	public static void diskLoad(TlbEntries t) throws FileNotFoundException {
+	public static void diskLoad(VirtualPageTableEntries t) throws FileNotFoundException {
 		
 	}
 	
 	public static void resetRbit(MMU mmu) {
 		mmu.resetRbits();
 		
-		for (int i = 0; i < clock.length(); ++i) {
-			clock.getData().setRbit(false);
-			clock.advance();
-		}
 	}
 	
 	public static void unsetDbit(MMU mmu, int virtualPageIndex) {
